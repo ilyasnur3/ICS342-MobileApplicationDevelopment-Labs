ac15f405444679010cdd5ea30118bc90fce4555c
Ilyas Nur <ilyasomarnur@gmail.com>
2023-08-06 19:04:52 -0500

    Implement NumbersRepository tests

--- app/src/test/java/com/ics342/labs/NumbersRepositoryTest.kt
+++ app/src/test/java/com/ics342/labs/NumbersRepositoryTest.kt
@@ -1,10 +1,13 @@
 package com.ics342.labs
 
 import io.mockk.every
+import io.mockk.just
 import io.mockk.mockk
+import io.mockk.runs
 import io.mockk.verify
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.Assertions.*
 import java.util.UUID
 import kotlin.random.Random
 
@@ -35,6 +38,30 @@ internal class NumbersRepositoryTest {
 
     @Test
     fun ifDatabaseIsEmptyShouldFetchNumbersFromApi() {
-        // TODO: implement this test
+        // Setup
+        val database = mockk<Database>()
+        val api = mockk<Api>()
+        val numbersFromApi = listOf(
+            Number(UUID.randomUUID().toString(), Random.nextInt()),
+            Number(UUID.randomUUID().toString(), Random.nextInt()),
+            Number(UUID.randomUUID().toString(), Random.nextInt())
+        )
+
+        every { database.getAllNumbers() } returns emptyList() // Mock database to return an empty list
+        every { api.getNumbers() } returns numbersFromApi // Mock API to return numbers
+        every { database.storeNumbers(numbersFromApi) } just runs // Mock database.storeNumbers to do nothing
+
+        // Act
+        val repository = NumbersRepository(database, api)
+        val result = repository.fetchNumbers()
+
+        // Assert
+        assertNotNull(result)
+        assertEquals(numbersFromApi, result)
+
+        // Verify that the required functions were called exactly once
+        verify(exactly = 1) { database.getAllNumbers() }
+        verify(exactly = 1) { api.getNumbers() }
+        verify(exactly = 1) { database.storeNumbers(numbersFromApi) }
     }
 }
